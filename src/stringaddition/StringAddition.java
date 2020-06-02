ba92726d3fc2660f1ea1282f0c5800d48bb0e241
My City Doctors <mycitydoctors.co.in@gmail.com>
2020-06-02 11:43:23 +0530

    Initial Commit

---
+++ src/stringaddition/StringAddition.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringaddition;
+
+/**
+ *
+ * @author dell
+ */
+public class StringAddition {
+    public int sum(String str){
+        if(str.isEmpty()){
+            return 0;
+        }
+        return 1;
+    }
+}
